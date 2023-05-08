a635050fb5ac987948aa2ee3cf0acae0a9dcc0b2
towficaziz <tapolly01@gmail.com>
2023-05-09 00:55:51 +0200

    Adding Spinner in Loging and Signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UIService } from '../shared/ui.service';
 
 @Injectable()
 export class AuthService {
@@ -17,7 +18,8 @@ export class AuthService {
   constructor(private router: Router,
     private afAuth: AngularFireAuth,
     private trainingService: TrainingService,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private uiService: UIService
     ){}
 
     initAuthListener(){
@@ -36,12 +38,15 @@ export class AuthService {
     }
 
   registerUser(authData: AuthData){
+    this.uiService.loadingStateChanged.next(true);
     this.afAuth.createUserWithEmailAndPassword(
       authData.email,
       authData.password
       ).then(result =>{
+    this.uiService.loadingStateChanged.next(false);
       })
       .catch(error =>{
+        this.uiService.loadingStateChanged.next(false);
         this.snackbar.open(error.message, null!, {
           duration: 3000
         });
@@ -49,13 +54,15 @@ export class AuthService {
   }
 
   login(authData: AuthData){
+    this.uiService.loadingStateChanged.next(true);
     this.afAuth.signInWithEmailAndPassword(
       authData.email,
       authData.password
       ).then(result =>{
-        console.log(result);
+        this.uiService.loadingStateChanged.next(false);
       })
       .catch(error =>{
+        this.uiService.loadingStateChanged.next(false);
         this.snackbar.open(error.message, null!, {
           duration: 3000
         });
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,18 +1,29 @@
+import { UIService } from './../../shared/ui.service';
 import { AuthService } from './../auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   maxDate!: Date;
+  isLoading = false;
+  private loadingSubs!: Subscription
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private uiService: UIService) { }
+
+  ngOnDestroy() {
+    this.loadingSubs.unsubscribe();
+  }
 
   ngOnInit() {
+    this.loadingSubs = this.uiService.loadingStateChanged.subscribe(isLoading =>{
+      this.isLoading = isLoading;
+    })
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
