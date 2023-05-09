65425b0162a1b39fb355d8172267ed8903547d48
towficaziz <tapolly01@gmail.com>
2023-05-10 00:05:50 +0200

    Spliting Up App-routing's Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,4 @@
 import { TrainingComponent } from './training/training.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -8,8 +6,6 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path:'', component: WelcomeComponent},
-  {path:'signup', component: SignupComponent},
-  {path:'login', component: LoginComponent},
   {path:'training', component: TrainingComponent, canActivate: [AuthGuard]}
 ];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { SignupComponent } from "./signup/signup.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { SharedModule } from "../shared/shared.module";
+import { AuthRoutingModule } from "./auth-routing.module";
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { SharedModule } from "../shared/shared.module";
   imports:[
     ReactiveFormsModule,
     AngularFireAuthModule,
-    SharedModule
+    SharedModule,
+    AuthRoutingModule
   ],
   exports:[]
 })
