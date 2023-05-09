2f17a62d2f7f046d72f99291aa9cba57b12097d0
towficaziz <tapolly01@gmail.com>
2023-05-09 13:54:09 +0200

    Addin a reUsable Snackbar(Notification)

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,6 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from "rxjs";
 
 
+@Injectable()
 export class UIService{
   loadingStateChanged = new Subject<boolean>();
+
+  constructor(private snackbar: MatSnackBar){}
+
+  showSnackbar(message: string, action: string , duration: any){
+    this.snackbar.open(message, action, {
+      duration: duration
+    });
+  }
 }
